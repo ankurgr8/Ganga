53921895f78f247924166bd9dbc609d8d9b14538
ankurgr8 <ankurgr8@users.noreply.github.com>
2020-08-26 14:14:04 +0530

    Experiment

---
+++ Experiment.js
@@ -0,0 +1,5 @@
+var abc = "Uniget Private Limited";
+var length = abc.search("L");
+var length1 = abc.("z");
+/*document.write(length);*/
+document.getElementById("Experiment").innerHTML = length + "<br>" + length1 ;
